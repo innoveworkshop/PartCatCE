b23c67ff5bf77401b71e5defbf8db111c62e632a
Nathan Campos <eeepc904@gmail.com>
2020-06-22 19:13:24 +0100

    Finally the TreeView is being populated correctly with categories and sub-categories

--- Sources/ComponentManager.cpp
+++ Sources/ComponentManager.cpp
@@ -59,28 +59,71 @@ void ComponentManager::PopulateTreeView() {
 			}
 		}
 
-		if (bUnique)
-			arrCategories.push_back(Category(szCategory));
+		// Looks like we have a new category.
+		if (bUnique) {
+			Category cat(szCategory);
+			cat.PopulateSubCategories(arrComponents);
+
+			arrCategories.push_back(cat);
+		}
 	}
 
 	// Add the category nodes and populate them with components.
 	for (i = 0; i < arrCategories.size(); i++) {
+		// Add category to the TreeView.
 		HTREEITEM nodeCategory = treeView->AddItem(NULL, arrCategories[i].GetName(),
-			NULL, 0, (LPARAM)i);
+			NULL, 0, (LPARAM)NULL);
 
-		// Go through components searching for the ones that belong here.
-		for (j = 0; j < arrComponents.size(); j++) {
-			LPCTSTR szCategory = arrComponents[j].GetCategory();
-			if (szCategory == NULL)
-				continue;
+		// Go through sub-categories and populate its components.
+		vector<wstring> arrSubCategories = arrCategories[i].GetSubCategories();
+		for (j = 0; j < arrSubCategories.size(); j++) {
+			HTREEITEM nodeSubCategory = treeView->AddItem(nodeCategory,
+				arrSubCategories[j].c_str(), NULL, 0, (LPARAM)NULL);
 
-			// This component belongs inside this category.
-			if (arrCategories[i].Equals(szCategory))
-				treeView->AddItem(nodeCategory, arrComponents[j].ToString(),
-					NULL, 0, (LPARAM)j);
+			// Go through components searching for the ones that belong here.
+			for (size_t k = 0; k < arrComponents.size(); k++) {
+				LPCTSTR szCategory = arrComponents[k].GetCategory();
+				if (szCategory == NULL)
+					continue;
+
+				// This component belongs inside this category.
+				if (arrCategories[i].Equals(szCategory)) {
+					LPCTSTR szSubCategory = arrComponents[k].GetSubCategory();
+					
+					// This component doesn't have a sub-category.
+					if (szSubCategory == NULL)
+						continue;
+
+					// Check if this component fits into this sub-category.
+					if (arrSubCategories[j].compare(wstring(szSubCategory)) == 0)
+						treeView->AddItem(nodeSubCategory, arrComponents[k].ToString(),
+							NULL, 0, (LPARAM)k);
+				}
+			}
+
+			// Expand the sub-category node.
+			treeView->ExpandNode(nodeSubCategory);
 		}
 
-		// Expand the node.
+		// Search for components without a sub-category.
+		if (arrCategories[i].HasComponentsWithNoSubCategory()) {
+			for (j = 0; j < arrComponents.size(); j++) {
+				LPCTSTR szCategory = arrComponents[j].GetCategory();
+				if (szCategory == NULL)
+					continue;
+
+				// This component belongs inside this category.
+				if (arrCategories[i].Equals(szCategory)) {
+					LPCTSTR szSubCategory = arrComponents[j].GetSubCategory();
+
+					if (szSubCategory == NULL)
+						treeView->AddItem(nodeCategory, arrComponents[j].ToString(),
+							NULL, 0, (LPARAM)j);
+				}
+			}
+		}
+
+		// Expand the category node.
 		treeView->ExpandNode(nodeCategory);
 	}
 
