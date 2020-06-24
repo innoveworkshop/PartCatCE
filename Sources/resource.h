f3bddd9ad91eb73d28cfbdc9668ece030bbf0090
Nathan Campos <eeepc904@gmail.com>
2020-06-24 15:03:46 +0100

    Added a spin control to the quantity edit

--- Sources/resource.h
+++ Sources/resource.h
@@ -17,6 +17,7 @@
 #define IDC_BTDATASHEET                 1004
 #define IDC_EDNOTES                     1005
 #define IDC_LSPROPS                     1006
+#define IDC_SPQUANTITY                  1007
 #define IDM_FILE_EXIT                   40002
 #define IDM_HELP_ABOUT                  40003
 
@@ -26,7 +27,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40004
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
